4f5f943acc30a6c4a5f0fc7c04cb7e415bb8a08b
OleksandrVoitko <voytkomail@gmail.com>
2022-03-09 12:51:19 +0200

    The task of saving to local storage is done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -58,6 +58,18 @@ class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts")
+    if (contacts) {
+      this.setState({ contacts: JSON.parse(contacts) });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const filteredContacts = this.getFilteredContacts();
